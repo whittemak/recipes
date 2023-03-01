064d2df28c7488c1bfe77ccda52d1e511366f14b
Dmitriy <whittemak@gmail.com>
2023-03-01 19:44:06 +0300

    первый коммит

---
+++ src/main/java/pro/sky/recipesapp/RecipesAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.recipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesAppApplication.class, args);
+    }
+
+}
